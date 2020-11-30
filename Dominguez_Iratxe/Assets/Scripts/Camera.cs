772b0d405b334b86309d8fb79e71fe9a066cc0cb
iratxedominguez2 <domingueziratxe7@gmail.com>
2020-11-30 17:07:08 +0100

    Tiempo transcurrido en el juego.

---
+++ Dominguez_Iratxe/Assets/Scripts/Camera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject esfera;
+    private Vector3 desplazamiento;
+    // Start is called before the first frame update
+    void Start()
+    {
+        desplazamiento = transform.position - esfera.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = esfera.transform.position + desplazamiento;
+
+    }
+}
---
+++ Dominguez_Iratxe/Assets/Scripts/Sphere.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sphere : MonoBehaviour
+   
+{
+    [SerializeField] Text myText;
+   
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Tiempo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+       
+    }
+    IEnumerator Tiempo()
+    {
+        int n;
+        for(n=0; ; n++)
+        {
+            myText.text = "Tiempo transcurrido:" + n;
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+   
+}
